87030bc3ae34a9f86119a4baab09fb2102c6196d
Pavanthedev <86460891+Pavanthedev@users.noreply.github.com>
2021-12-30 09:38:48 +0530

    changes1

--- index.php
+++ index.php
@@ -30,6 +30,10 @@
     font-size: 27px;
     top: 0;
 }
+
+#class{
+  align-items:center;
+}
 #viewer_modal .modal-dialog {
         width: 80%;
     max-width: unset;
@@ -61,6 +65,7 @@ a.jqte_tool_label.unselectable {
 a.jqte_tool_label.unselectable {
     height: 22px !important;
 }*/
+
     </style>
     <body id="page-top">
         <!-- Navigation-->
@@ -180,13 +185,27 @@ a.jqte_tool_label.unselectable {
                         <!-- Make sure to change the email address in BOTH the anchor text and the link target below!-->
                         <a class="d-block" href="mailto:<?php echo $_SESSION['system']['email'] ?>"><?php echo $_SESSION['system']['email'] ?></a>
                     </div>
+                    <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
+  <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+  <path fill-rule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"/>
+  <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
+</svg>
+<br>
+<br>
+                    Developed by Lakshmi Pavan Rangisetty, Vatti Anudeep & Kruthik Seelamsetty
+                    </div>
+                    
                 </div>
+                
             </div>
             <br>
+          
             <div class="container"><div class="small text-center text-muted">Copyright © 2021 - <?php echo $_SESSION['system']['name'] ?> | <a href="https://www.tatvaglobalschool.com" target="_blank">Visit School website</a></div></div>
         </footer>
         
        <?php include('footer.php') ?>
+       
     </body>
     <script type="text/javascript">
       $('#login').click(function(){
